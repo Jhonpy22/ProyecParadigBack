939ee581ae73f9ff45d5d8f931631ff471198127
Jhonn Rojas <jhonngranadosrojas@email.com>
2025-09-21 15:56:30 -0600

    Cambios en turnos

--- Domain/States/Turnos/DesocupadoState.cs
+++ Domain/States/Turnos/DesocupadoState.cs
@@ -9,8 +9,13 @@ namespace Domain.States.Turnos
     {
         public Task VoltearAsync(Partida j, int jugadorId, int indice)
         {
+
+            var tiempoTranscurrido = (DateTime.UtcNow - j.IniciadaUtc).TotalSeconds;
+            Console.WriteLine($"TIEMPO CHECK - Transcurrido: {tiempoTranscurrido:F1}s de {j.DuracionSegundos}s, Expirada: {j.Expirada(DateTime.UtcNow)}");
+
             if (FinalizacionDePartida.DebeExpirar(j, DateTime.UtcNow))
             {
+                Console.WriteLine("PARTIDA EXPIRADA - Finalizando por tiempo agotado");
                 FinalizacionDePartida.FinalizarYVolverAlLobby(j);
                 return Task.CompletedTask;
             }
